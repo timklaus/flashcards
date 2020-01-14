e9c3c7d78326ecfd7862e339b18dc814d514fb60
Student <example@example.com>
2020-01-14 14:14:13 -0800

    impliment javascript to interact with cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,42 @@
 $(document).ready(function() {
-  $("h3").click(function() {
-    $("li").toggle();
-   
+  //this is the fonction for the operators
+  $("#operators").click(function() {
+    $("#1").fadeToggle();
   });
+
+  // this is the fonction for the 
+  $("#javascript").click(function() {
+    $("#2").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#variables").click(function() {
+    $("#3").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#functions").click(function() {
+    $("#4").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#strings").click(function() {
+    $("#5").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#alerts").click(function() {
+    $("#6").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#attributes").click(function() {
+    $("#7").fadeToggle();
+  });
+
+  // this is the fonction for the 
+  $("#jquery").click(function() {
+    $("#8").fadeToggle();
+  });
+
 });
