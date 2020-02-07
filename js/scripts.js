a15a3f87ececa753102ae90c30d2677b6eb8314f
Student <example@example.com>
2020-02-06 20:44:56 -0800

    Modified CSS to style the page with colors and fonts, added a new row of flash cards, added definitions to carts 1-7

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,58 @@
 $(document).ready(function() {
   //this is the fonction for the operators
   $("#operators").click(function() {
-    $("#1").fadeToggle();
+    $("#1").slideToggle();
   });
 
   // this is the fonction for the 
   $("#javascript").click(function() {
-    $("#2").fadeToggle();
+    $("#2").slideToggle();
   });
 
   // this is the fonction for the 
   $("#variables").click(function() {
-    $("#3").fadeToggle();
+    $("#3").slideToggle();
   });
 
   // this is the fonction for the 
   $("#functions").click(function() {
-    $("#4").fadeToggle();
+    $("#4").slideToggle();
   });
 
   // this is the fonction for the 
   $("#strings").click(function() {
-    $("#5").fadeToggle();
+    $("#5").slideToggle();
   });
 
   // this is the fonction for the 
   $("#alerts").click(function() {
-    $("#6").fadeToggle();
+    $("#6").slideToggle();
   });
 
   // this is the fonction for the 
   $("#attributes").click(function() {
-    $("#7").fadeToggle();
+    $("#7").slideToggle();
   });
 
   // this is the fonction for the 
   $("#jquery").click(function() {
-    $("#8").fadeToggle();
+    $("#8").slideToggle();
+  });
+  // this is the function for the Methods Card
+  $("#methods").click(function() {
+    $("#9").slideToggle();
+  });
+  // this is the function for the Arguments card
+  $("#arguments").click(function() {
+    $("#10").slideToggle();
+  });
+  // this is the function for the Parameters Card
+  $("#parameters").click(function() {
+    $("#11").slideToggle();
+  });
+  // this is the function for the Booleans Card
+  $("#booleans").click(function() {
+    $("#12").slideToggle();
   });
 
 });
